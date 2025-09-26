f04c2aea6b80f4909008539c1d94f2f163f00832
Shamiul40 <shamiul4040@gmail.com>
2025-09-26 17:25:48 +0600

    home page done

--- app/page.js
+++ app/page.js
@@ -1,17 +1,35 @@
 import Image from "next/image";
 import bgImage from "@/public/home/Bg.jpg"
+import Link from "next/link";
+
+import {SparklesIcon, SpeakerXMarkIcon} from "@heroicons/react/24/solid"
 
 export default function Home() {
   return (
-    <div>
-      <Image className="blur-sm" src={bgImage}
+    <div className="flex flex-col justify-center items-center h-screen  relative">
+      <Image className="blur-xs" src={bgImage}
       alt="Background Image"
       fill
       sizes="100vw"
       style={{objectFit:"cover"}}
       >
-
+        
       </Image>
+
+
+      <div className="z-10 p-2 flex flex-col justify-center items-center   rounded-lg bg-purple-100 md:w-2/5 h-2/4">
+
+        <div className="text-purple-800 flex pb-5 mb-5 text-5xl ">
+          <SparklesIcon className="w-12 h-12 p-2" />
+          <p >LitLoop</p>
+        </div>
+        <div>
+          <Link className="bg-purple-700 p-2 rounded-lg text-white" href="/store">
+            Take me to the store
+          </Link>
+        </div>
+
+      </div>
     </div>
   );
 }
